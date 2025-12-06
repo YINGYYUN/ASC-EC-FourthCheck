04ef349a6303fd0c6d01f1ed4cb2c3fc1347e761
YINGYYUN <756209834@qq.com>
2025-12-06 13:50:18 +0800

    feat:按键模块完成

--- User/main.c
+++ User/main.c
@@ -1,6 +1,10 @@
 #include "stm32f10x.h"                  // Device header
+
 #include "Delay.h"
+#include "Timer.h"
+
 #include "OLED.h"
+#include "Key.h"
 
 /**
   * 坐标轴定义：
@@ -26,12 +30,81 @@
 int main(void)
 {
 	OLED_Init();
+	Key_Init();
 	
-	OLED_Clear();	
+	Timer_Init();
+	
+/* =================== [START] 菜单初始化模块 [START] =================== */	
+	char Main_Menu[][10] = {"ADC", "Store", "IMU"};
+	#define F_Mian_Menu				0
+	#define F_ADC_Mode				1
+	#define F_Store_Mode			2
+	#define F_IMU_Mode			2
+	//光标位置
+	uint8_t Main_Menu_Location = F_ADC_Mode;
+	//功能状态
+	uint8_t FUNCTION_State = F_Mian_Menu;
+	
+	OLED_Clear();
+	
+	OLED_Printf(16, 0, OLED_8X16, Main_Menu[0]);
+	OLED_Printf(16, 16, OLED_8X16, Main_Menu[1]);
+	OLED_Printf(16, 32, OLED_8X16, Main_Menu[2]);
+	
+	OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");
+	
 	OLED_Update();
 	
-	while (1)
-	{
+/* =================== [END] 菜单初始化模块 [END] =================== */	
 	
+	uint16_t i = 0;
+	
+	while (1)
+	{	
+		// 上键单击：菜单上移
+		if(Key_Check(KEY_NAME_UP, KEY_SINGLE))
+		{
+			// 上移逻辑
+			i += 1000;
+		}
+	
+		// 下键单击：菜单下移
+		else if(Key_Check(KEY_NAME_DOWN, KEY_SINGLE))
+		{
+			// 下移逻辑
+			i += 100;
+		}
+		
+		if(Key_Check(KEY_NAME_CONFIRM, KEY_SINGLE))
+		{
+		// 菜单切换逻辑
+			i += 1;
+		}	
+		
+		// 确认键长按：保存数据（复用模板的KEY_LONG标志位）
+		else if(Key_Check(KEY_NAME_CONFIRM, KEY_LONG))
+		{
+			// 数据保存逻辑
+			i += 10;
+		}
+	
+		
+		
+		
+		
+		OLED_Printf(0, 48, OLED_8X16, "%d", i);
+		OLED_Update();
+	}
+}
+
+//1ms的定时器定时中断
+void TIM1_UP_IRQHandler(void)
+{
+	//检查标志位
+	if (TIM_GetITStatus(TIM1,TIM_IT_Update) == SET )
+	{
+		Key_Tick();
+		//清除标志位
+		TIM_ClearITPendingBit(TIM1,TIM_IT_Update);
 	}
 }
