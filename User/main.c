6e9c8750f2fbcdcfe94c3781f4e55cbde9ee0cb8
YINGYYUN <756209834@qq.com>
2025-12-06 14:24:02 +0800

    feat:2025.12.6完成菜单架构

--- User/main.c
+++ User/main.c
@@ -36,64 +36,121 @@ int main(void)
 	
 /* =================== [START] 菜单初始化模块 [START] =================== */	
 	char Main_Menu[][10] = {"ADC", "Store", "IMU"};
-	#define F_Mian_Menu				0
-	#define F_ADC_Mode				1
-	#define F_Store_Mode			2
-	#define F_IMU_Mode			2
+	uint16_t Value_ADC_Mode[] = {1, 2, 3};
+	uint16_t Value_Store_Mode[] = {4, 5, 6};
+	uint16_t Value_IMU_Mode[] = {7, 8, 9};
+	#define Flag_Mian_Menu				0
+	#define Flag_ADC_Mode				1
+	#define Flag_Store_Mode			2
+	#define Flag_IMU_Mode				3
 	//光标位置
-	uint8_t Main_Menu_Location = F_ADC_Mode;
+	uint8_t Main_Menu_Location = Flag_ADC_Mode;
 	//功能状态
-	uint8_t FUNCTION_State = F_Mian_Menu;
+	uint8_t FUNCTION_State = Flag_Mian_Menu;
 	
 	OLED_Clear();
-	
-	OLED_Printf(16, 0, OLED_8X16, Main_Menu[0]);
-	OLED_Printf(16, 16, OLED_8X16, Main_Menu[1]);
-	OLED_Printf(16, 32, OLED_8X16, Main_Menu[2]);
-	
-	OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");
-	
-	OLED_Update();
-	
+	OLED_Printf(8, 0, OLED_8X16, Main_Menu[0]);
+	OLED_Printf(8, 16, OLED_8X16, Main_Menu[1]);
+	OLED_Printf(8, 32, OLED_8X16, Main_Menu[2]);
+	OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");	
+//	OLED_Printf(112, 0, OLED_8X16, "+");
+	OLED_Update();	
 /* =================== [END] 菜单初始化模块 [END] =================== */	
 	
-	uint16_t i = 0;
 	
 	while (1)
 	{	
 		// 上键单击：菜单上移
 		if(Key_Check(KEY_NAME_UP, KEY_SINGLE))
 		{
-			// 上移逻辑
-			i += 1000;
+			if (FUNCTION_State == Flag_Mian_Menu)
+			{
+				OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, " ");
+				Main_Menu_Location = (Main_Menu_Location - 1 - 1 + 3) % 3 + 1;
+				OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");			
+				OLED_Update();
+			}
 		}
 	
 		// 下键单击：菜单下移
 		else if(Key_Check(KEY_NAME_DOWN, KEY_SINGLE))
 		{
-			// 下移逻辑
-			i += 100;
+			if (FUNCTION_State == Flag_Mian_Menu)
+			{
+				OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, " ");
+				Main_Menu_Location = (Main_Menu_Location - 1 + 1) % 3 + 1;
+				OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");		
+				OLED_Update();
+			}
 		}
 		
-		if(Key_Check(KEY_NAME_CONFIRM, KEY_SINGLE))
+		else if(Key_Check(KEY_NAME_CONFIRM, KEY_SINGLE))
 		{
+			if (FUNCTION_State == Flag_Mian_Menu)
+			{
+				FUNCTION_State = Main_Menu_Location;
+				
+				//显示更新
+				switch(FUNCTION_State)
+				{
+					case Flag_ADC_Mode:
+						OLED_Clear();
+						OLED_Printf(8, 16, OLED_8X16, "POT   val:%d",Value_ADC_Mode[0]);
+						OLED_Printf(8, 32, OLED_8X16, "NTC   val:%d",Value_ADC_Mode[1]);
+						OLED_Printf(8, 48, OLED_8X16, "LDR   val:%d",Value_ADC_Mode[2]);
+						OLED_Update();
+					
+						break;
+					
+					case Flag_Store_Mode:
+						OLED_Clear();
+						OLED_Printf(8, 16, OLED_8X16, "POT   val:%d",Value_Store_Mode[0]);
+						OLED_Printf(8, 32, OLED_8X16, "NTC   val:%d",Value_Store_Mode[1]);
+						OLED_Printf(8, 48, OLED_8X16, "LDR   val:%d",Value_Store_Mode[2]);
+						OLED_Update();
+					
+					
+						break;
+					
+					case Flag_IMU_Mode:
+						OLED_Clear();
+						OLED_Printf(8, 16, OLED_8X16, "Pitch:%d",Value_IMU_Mode[0]);
+						OLED_Printf(8, 32, OLED_8X16, "Roll:%d",Value_IMU_Mode[1]);
+						OLED_Printf(8, 48, OLED_8X16, "Yaw:%d",Value_IMU_Mode[2]);
+						OLED_Update();
+					
+					
+						break;
+					
+					
+					
+				}
+				
+			}
+			else
+			{
+				FUNCTION_State = Flag_Mian_Menu;
+				
+				//显示更新
+				OLED_Clear();
+				OLED_Printf(8, 0, OLED_8X16, Main_Menu[0]);
+				OLED_Printf(8, 16, OLED_8X16, Main_Menu[1]);
+				OLED_Printf(8, 32, OLED_8X16, Main_Menu[2]);			
+				OLED_Printf(0, 16 * (Main_Menu_Location - 1) , OLED_8X16, ">");			
+				OLED_Update();	
+			}
 		// 菜单切换逻辑
-			i += 1;
+			
 		}	
 		
 		// 确认键长按：保存数据（复用模板的KEY_LONG标志位）
 		else if(Key_Check(KEY_NAME_CONFIRM, KEY_LONG))
 		{
 			// 数据保存逻辑
-			i += 10;
 		}
 	
 		
-		
-		
-		
-		OLED_Printf(0, 48, OLED_8X16, "%d", i);
-		OLED_Update();
+
 	}
 }
 
